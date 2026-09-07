a8ccdd29df77f0b0fb5998a932f8dbad33ec703b
duendekan788 <70450086+duendekan788@users.noreply.github.com>
2026-09-07 07:13:08 +0200

    Update PAYPAL_TEST constant to new value

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 const MIN_AMOUNT = 5;
 const MAX_AMOUNT = 10000;
 const WORKER_VERSION = "DEBUG-2026-09-07-B";
-const PAYPAL_TEST = "OAUTH-TEST-02";
+const PAYPAL_TEST = "OAUTH-TEST-03";
 
 function json(data, status = 200) {
   return new Response(JSON.stringify(data), {

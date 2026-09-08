dbcdfbe3e97f73bccfd16d889ab7043018e288f9
duendekan788 <70450086+duendekan788@users.noreply.github.com>
2026-09-08 04:07:52 +0200

    Add PayPal configuration handler

--- netlify/functions/paypal-config.js
+++ netlify/functions/paypal-config.js
@@ -1,1 +1,17 @@
 
+exports.handler = async event => {
+  return {
+    statusCode: 200,
+
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store",
+      "Access-Control-Allow-Origin": "*"
+    },
+
+    body: JSON.stringify({
+      clientId: process.env.PAYPAL_CLIENT_ID || "",
+      mode: process.env.PAYPAL_MODE || "sandbox"
+    })
+  };
+};
